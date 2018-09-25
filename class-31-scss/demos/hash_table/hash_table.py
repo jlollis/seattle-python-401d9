effcdcc97148255a8a4b4f0d95a8bffeba5bfd51
Scott Schmidt <sjschmidt44@gmail.com>
2018-09-24 19:19:43 -0700

    adds hash table scaffold to demos for class31

---
+++ class-31-scss/demos/hash_table/hash_table.py
@@ -0,0 +1,47 @@
+class HashTable:
+    """A class for a hash table."""
+    entries_count = 0
+    alphabet_size = 52
+
+    def __init__(self, size=8192):
+        self.table_size = size
+        self.hashtable = []
+
+    def __repr__(self):
+        pass
+
+    def _hash_key(self, key):
+        """Create a hash from a given key.
+        args:
+            key: a string to hash
+        returns: an integer corresponding to hashtable index
+        """
+        hash_ = 0
+        for i, c in enumerate(key):
+            hash_ += pow(
+                self.alphabet_size, len(key) - i - 1) * ord(c)
+        return hash_ % self.table_size
+
+    def set(self, key, value):
+        """Add a key and value into the hash table.
+        args:
+            key: the key to store
+            value: the value to store
+        """
+        pass
+
+    def get(self, key):
+        """Retrieve a value from the hash table by key.
+        args:
+            key: a string to find the value in the hash table
+        returns: the value stored with the key
+        """
+        pass
+
+    def remove(self, key):
+        """Retrieve and remove a value from the hash table by key.
+        args:
+            key: a string to find the value in the hash table
+        returns: the value stored with the key
+        """
+        pass
