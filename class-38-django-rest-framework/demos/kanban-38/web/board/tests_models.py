0f9320000020c23c09b30dcd0f7ab4c6858d7e87
Cris Ewing <cris@crisewing.com>
2018-10-03 13:48:58 -0700

    add kanban app demo code for class 38

---
+++ class-38-django-rest-framework/demos/kanban-38/web/board/tests_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from ..kanban_project.factories import UserFactory, CategoryFactory, CardFactory
+
+
+class TestCategoryModels(TestCase):
+    def setUp(self):
+        self.category = CategoryFactory(
+            name='test name',
+            description='test desc'
+        )
+
+    def test_default_category_attrs(self):
+        self.assertEqual(self.category.name, 'test name')
+        self.assertEqual(self.category.description, 'test desc')
+
+
+class TestCardModels(TestCase):
+    pass
